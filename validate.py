af4b9517b1d6008d84230fe68770bf3de23d2ec6
rajnishspandey <pandeyrajnish86@yahoo.com>
2024-02-20 12:07:44 +0530

    updated date format and also buttons for small devices

--- validate.py
+++ validate.py
@@ -1,5 +1,12 @@
 import pytz
 
+def add_ordinal(day):
+    if 10 <= day % 100 <= 20:
+        suffix = 'th'
+    else:
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
+    return f"{day}{suffix}"
+
 def convert_gmt_to_ist(gmt_time):
     gmt_timezone = pytz.timezone('GMT')
     ist_timezone = pytz.timezone('Asia/Kolkata')  # Indian Standard Time
@@ -8,6 +15,6 @@ def convert_gmt_to_ist(gmt_time):
     ist_time = gmt_time.astimezone(ist_timezone)
 
     # Format IST time to show only time
-    ist_time_str = ist_time.strftime('%Y-%m-%d %H:%M')
+    ist_time_str = ist_time.strftime(f"{add_ordinal(ist_time.day)}-{ist_time.strftime('%b')[:3]}-%y %H:%M").lower()
 
     return ist_time_str
