32b832359d841618ec58a47b85888eddfd5dd07c
rajnishspandey <pandeyrajnish86@yahoo.com>
2024-02-25 18:36:04 +0530

    beta release 1.3.5

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 
 > beta release 1.3.4
 >>  correction done for geolocation
+
+> beta release 1.3.5
+>>  added Zimbabwe in the list and Bussines category
+>> updated the code which will not redirect to google if url found it will directly go to source url other wise through google
--- getNews.py
+++ getNews.py
@@ -7,8 +7,12 @@ from string_literals import JSON_PATH, BASE_URL, POST_ON_X_URL, GEO_LOCATION
 import urllib.request
 import json
 import ssl
+import requests
+import concurrent.futures
 
 ssl_context = ssl._create_unverified_context()
+
+
 def get_location():
     with urllib.request.urlopen(GEO_LOCATION, context=ssl_context) as url:
         geo_data = json.loads(url.read().decode())
@@ -25,13 +29,25 @@ def get_location():
 
     return location_data
 
+
+def fetch_content(article_url):
+    try:
+        content_response = requests.get(article_url)
+        if content_response.status_code == 200:
+            return content_response.url
+    except Exception as e:
+        pass
+
+    return article_url
+
+
 def getResult():
     # Read configure from JSON file
     with open(JSON_PATH, 'r') as file:
         configure = json.load(file)
 
     geo_location = get_location()
-    
+
     geo_country_code = geo_location.get('country_code')
     country = request.form.get('cnt', request.args.get('cnt', geo_country_code))
     category = request.form.get('ctgry', request.args.get('ctgry', None))
@@ -48,31 +64,41 @@ def getResult():
         f"{base_url}/search?q={encoded_category}&hl={language_param}-{country}&gl={country}&ceid={country}:{language_param}" if country and category else
         base_url
     )
-    print(f"inside else : - {rss_feed_url}")
     feed = feedparser.parse(rss_feed_url)
-    
+
     processed_results = []
 
-    for entry in feed.entries:
-        # Convert GMT time to IST with offset
-        gmt_time = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
-        ist_date_time_str = convert_gmt_to_ist(gmt_time)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(fetch_content, entry.link) for entry in feed.entries]
 
-        selected_country_name = next((c['name'] for c in configure['countries'] if c['gl'] == country), None)
+        for entry, future in zip(feed.entries, concurrent.futures.as_completed(futures)):
+            try:
+                content_url = future.result()
+            except Exception as e:
+                content_url = entry.link
 
-        processed_results.append({
-            'news_url': entry.link,
-            'news_text': entry.title,
-            'published_date_time_gmt': gmt_time,  # Original GMT time
-            'published_date_time_ist': ist_date_time_str + ' IST',
-            'category': category if category else "All News",
-            'country': selected_country_name,
-            'post_on_x': POST_ON_X_URL
-        })
+            # Convert GMT time to IST with offset
+            gmt_time = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
+            ist_date_time_str = convert_gmt_to_ist(gmt_time)
+
+            selected_country_name = next(
+                (c['name'] for c in configure['countries'] if c['gl'] == country), None)
+
+            processed_results.append({
+                'news_url': content_url,
+                'news_text': entry.title,
+                'published_date_time_gmt': gmt_time,  # Original GMT time
+                'published_date_time_ist': ist_date_time_str + ' IST',
+                'category': category if category else "All News",
+                'country': selected_country_name,
+                'post_on_x': POST_ON_X_URL
+            })
 
     return processed_results, configure
 
+
 def getNewsProcessed(processed_results):
-    processed_results = sorted(processed_results, key=lambda x: x['published_date_time_gmt'], reverse=True)
+    processed_results = sorted(
+        processed_results, key=lambda x: x['published_date_time_gmt'], reverse=True)
 
     return processed_results
